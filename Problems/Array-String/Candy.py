cb1128408b2b18871eb6b7764453b056780fc23a
teyotobias <teyotobias@utexas.edu>
2023-11-20 15:29:50 -0600

    Candy

--- Problems/Array-String/Candy.py
+++ Problems/Array-String/Candy.py
@@ -1,14 +1,16 @@
 class Solution:
     def candy(self, ratings):
         n = len(ratings)
-        candies = [1] * n 
+        candy = [1] * n
+        if n == 1:
+            return 1
+        #front pass -> checking if greater than to the right
+        for i in range(n-1):
+            if ratings[i+1] > ratings[i]:
+                candy[i+1] = max(candy[i+1], candy[i] + 1)
+        # back pass -> checking if rating greater than to the left
+        for i in range(n-1, 0, -1):
+            if ratings[i-1] > ratings[i]:
+                candy[i-1] = max(candy[i-1], candy[i] + 1) 
 
-        for i in range(1, n):
-            if ratings[i] > ratings[i-1]:
-                candies[i] = candies[i-1] + 1
-
-        for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1]:
-                candies[i] = max(candies[i], candies[i+1] + 1)
-        
-        return sum(candies)
+        return sum(candy)
