135b27e7060957a377436365faf703aeaa867177
teyotobias <teyotobias@utexas.edu>
2023-12-06 19:52:28 -0600

    Simple linkedlist problem

---
+++ Problems/LinkedList/LinkedListCycle/LinkedListCycle.py
@@ -0,0 +1,9 @@
+
+
+def hasCycle(head):
+    if not head or not head.next: return False        
+    slow, fast = head, head.next
+    while slow != fast:
+        if not fast or not fast.next: return False
+        slow, fast = slow.next, fast.next.next
+    return True
