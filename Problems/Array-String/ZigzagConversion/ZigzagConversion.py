30a2e78b3bf9c6e0ac0cd906981cacc87de4d32f
teyotobias <teyotobias@utexas.edu>
2023-12-15 01:51:33 -0600

    Zigzag conversion and linkedlistcycle, both gave issues.

--- Problems/Array-String/ZigzagConversion/ZigzagConversion.py
+++ Problems/Array-String/ZigzagConversion/ZigzagConversion.py
@@ -1,4 +1,18 @@
-class Solution:
-    
-    def convert(s, numRows):
-        return -1
+# Runtime: 54 ms, faster than 81.06% of Python3 online submissions for Zigzag Conversion.
+# Memory Usage: 16.2 MB, less than 91.16% of Python3 online submissions for Zigzag Conversion.
+def convert(s, numRows):
+    if numRows == 1 or numRows >= len(s):
+        return s
+    ret = [''] * numRows
+    going_down = False
+    currRow = 0
+    for char in s:
+        ret[currRow] += char
+        if currRow == 0 or currRow == (numRows - 1):
+            going_down = not going_down
+        if going_down:
+            currRow += 1
+        else:
+            currRow -= 1
+    return ''.join(ret)
+            
--- Problems/LinkedList/LinkedListCycle/LinkedListCycle.py
+++ Problems/LinkedList/LinkedListCycle/LinkedListCycle.py
@@ -1,5 +1,6 @@
 #have not done yet
-
+# Runtime: 55 ms, faster than 73.02% of Python3 online submissions for Linked List Cycle.
+# Memory Usage: 20.5 MB, less than 67.99% of Python3 online submissions for Linked List Cycle.
 def hasCycle(head):
     if not head or not head.next: return False        
     slow, fast = head, head.next
