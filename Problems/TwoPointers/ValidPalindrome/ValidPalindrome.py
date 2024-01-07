93eaab7f09e850a266b7bb98b6bc0dee8e677775
teyotobias <teyotobias@utexas.edu>
2024-01-06 21:46:37 -0600

    some practice

--- Problems/TwoPointers/ValidPalindrome/ValidPalindrome.py
+++ Problems/TwoPointers/ValidPalindrome/ValidPalindrome.py
@@ -16,3 +16,19 @@ class Solution:
             l += 1
             r -= 1
         return True
+    
+
+
+    def isPali(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            while not s[left].isalnum() and left < right:
+                left += 1
+            while not s[right].isalnum() and left < right:
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+
